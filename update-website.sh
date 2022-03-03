65eee61f91909b706db3f1e765cfd5eaa442717f
Jeremy Scheuerman <60898774+therenow9@users.noreply.github.com>
2022-03-03 14:18:38 -0500

    Update update-website.sh

--- update-website.sh
+++ update-website.sh
@@ -1,3 +1,4 @@
+#run this version as sudo bash update-website.sh
 cd home/linux_user/Downloads
 sudo -u linux_user git clone git@github.com:github_user/personal-site.git
 # if the repo is private the ssh keys are to be associated with a certain user on the server
