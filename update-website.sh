2a3df62822b4af9fa048262433ef279cc085b5f4
Jeremy Scheuerman <60898774+therenow9@users.noreply.github.com>
2022-03-03 14:14:55 -0500

    Delete update-website.sh

--- update-website.sh
+++
@@ -1,5 +0,0 @@
-cd home/linux_user/Downloads
-sudo -u linux_user git clone git@github.com:github_user/personal-site.git
-# if the repo is private the ssh keys are to be associated with a certain user on the server
-cp -r -f personal-site /var/www
-sudo rm -r personal-site
